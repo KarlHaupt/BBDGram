117a6fbd544cf447270d6b4fc61033660109ec37
Karl Haupt <karl@bbd.co.za>
2023-08-16 18:19:13 +0200

    Added Routing

--- frontend/src/src/App.tsx
+++ frontend/src/src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-     {/* Entry Point to the App */}
-     {/* Normally the navigation goes here, I can handle if you guys need me to */}
-     <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Entry Point to the App */}
+          {/* Normally the navigation goes here, I can handle if you guys need me to */}
+            <Route path = "/" Component={Home} />
+            <Route path = "/header" Component={Header} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
