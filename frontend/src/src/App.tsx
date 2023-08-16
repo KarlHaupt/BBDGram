bbe9af9053ae9a3f5b2e710dc29199c5106467d6
Karl Haupt <karl@bbd.co.za>
2023-08-16 17:34:11 +0200

    Setting up Frontend - React

---
+++ frontend/src/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+     {/* Entry Point to the App */}
+     {/* Normally the navigation goes here, I can handle if you guys need me to */}
+     <Home />
+    </div>
+  );
+}
+
+export default App;
