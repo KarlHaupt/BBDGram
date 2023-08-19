0f39874ff9425bcf7d8de419706c6164f2992be5
Stephen <slpotgieter1@gmail.com>
2023-08-19 18:11:20 +0100

    created header and added new post and bio buttons

--- frontend/src/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/src/components/UserProfile/UserProfile.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
+import '../UserProfile/UserProfile.css';
 
 const UserProfile = () => {
+  const [description, setDescription] = useState("Bio description goes here. Something about me.");
+
+  const handleEditDescription = () => {
+    const newDescription = prompt('Enter new description:');
+    if (newDescription !== null) {
+      setDescription(newDescription);
+    }
+  };
+
   return (
-    < Header />
+    <div>
+      <Header />
+      <div className="user-profile">
+        <div className="profile-header">
+          <img
+            src={require('../../images/sample-profile.jpg')}
+            alt="Profile"
+            className="profile-picture"
+          />
+          <div className="profile-details">
+            <h2 className='username'>Stephen Potgieter</h2>
+            <p className='description'>{description}</p>
+            <div className='settings'>
+              <button className='buttons' onClick={handleEditDescription}>Edit Description</button>
+              <button className='buttons'>New Post</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
-export default UserProfile
+
+export default UserProfile;
