f8da7685997b496b21857478d2caf29315109afe
DanielSBBD <daniels@bbd.co.za>
2023-09-05 23:31:18 +0200

    Added endpoint to update the user's username

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+export interface IUser {
+    username: string;
+    email: string;
+}
+
+const userSchema: Schema = new Schema({
     username: {
         type: String,
-        required: [true, 'Please enter your name'],
-        maxLength: [30, 'Your name cannot exceed 30 characters']
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Your name cannot exceed 30 characters"]
     },
-    password: {
+    email: {
         type: String,
-        required: [true, 'Please enter your password'],
-        minlength: [6, 'Your password must be at least 6 characters'],
-        select: false //Dont display password
+        required: [true, "Please enter your email"],
+        minLength: [6, "Your email must be at least 6 characters"],
+        maxLength: [320, "Your email cannot be longer than 320 characters"],
+        unique: true
     },
-
 });
 
-export default mongoose.model('User', userSchema );
+export default mongoose.model<IUser>("User", userSchema );
