8735774714aa7338f87d3c7d7eef841e8ed135fd
DanielSBBD <daniels@bbd.co.za>
2023-09-07 22:46:34 +0200

    Added home page

--- frontend/src/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/src/components/UserProfile/UserProfile.tsx
@@ -5,6 +5,7 @@ import { ApiResponse } from '../../providers/userProvider';
 import config from '../../config.json';
 import { Buffer } from 'buffer';
 import { Post } from '../../models/posts';
+import PicturePopup from '../PicturePopup/PicturePopup';
 
 const UserProfile = () => {
   const [description, setDescription] = useState("Bio description goes here. Something about me.");
@@ -24,15 +25,6 @@ const UserProfile = () => {
 
   const handlePostClick = (post: Post) => {
     setSelectedPost(post);
-  };
-
-  const countLikes = (post: any)=> {
-    return post.likes.length;
-  };
-
-  
-  const countDislikes = (post:any) => {
-    return post.dislikes.length;
   };
 
   const updateLikes = (postId: string, userId: string) => {
@@ -220,20 +212,12 @@ const UserProfile = () => {
           </div>
         ))}
       </div>
-      {selectedPost && (
-    <div className="modal">
-      <div className="modal-content">
-        <img src={`data:image/png;base64,${Buffer.from(selectedPost.image.data.data).toString('base64')}`} alt={`Post ${selectedPost._id}`} className="modal-image" />
-        <div className="modal-details">
-          <p>{selectedPost.description}</p>
-          <p>Likes: {countLikes(selectedPost)}</p><span><button className="like-button" onClick={handleLike}>Like</button></span>
-          <p>Dislikes: {countDislikes(selectedPost)}</p><span><button className="dislike-button" onClick={handleDislike}>Dislike</button></span>
-          
-        </div>
-        <button className="modal-close" onClick={() => setSelectedPost(null)}>Close</button>
-      </div>
-    </div>
-  )}
+      <PicturePopup 
+        selectedPost={selectedPost}
+        setSelectedPost={setSelectedPost}
+        handleLike={handleLike}
+        handleDislike={handleDislike}
+      />
     </div>
   );
 };
